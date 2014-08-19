6a785a10253649a72e6896e27e113a1477f4b99b
Gary Kagan <garykagan@Garys-MacBook-Pro.local>
2014-08-19 09:25:52 -0400

    Added vim gutter plugin

--- keepme.sh
+++ keepme.sh
@@ -59,6 +59,7 @@ then
         "https://github.com/terryma/vim-multiple-cursors.git" \
         "https://github.com/honza/vim-snippets.git" \
         "https://github.com/kchmck/vim-coffee-script" \
+        "https://github.com/airblade/vim-gitgutter" \
         "https://github.com/tpope/vim-surround.git"
 do
     git clone $pos
